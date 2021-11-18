7df9dea214a12aaa3cf447a1420ae5fc034438fa
Natasha <njohnson533@gmail.com>
2021-11-17 19:20:27 -0500

    styled navbar

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,9 +1,15 @@
 export function Hamburger() {
     return (
         <section className="hamburger__container">
-            <div className="hamburger"></div>
+            <div className="hamburger" onClick={toggleHamburger}></div>
         </section>
     );
 }
+
+function toggleHamburger(e) {
+    // e.currentTarget.classList.toggle("open");
+    document.querySelector(".nav__links").classList.toggle("show");
+}
+
 
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,31 +7,29 @@ export function Navbar() {
 				<section className="nav__brand">
 					<h1>&lt;NJ&gt;</h1>
 				</section>
-				<section className="nav__container">
-					<ul className="nav__links">
-						<li>
-							<a href="#home" className="nav__link">
-								Home
-							</a>
-						</li>
-						<li>
-							<a href="#about" className="nav__link">
-								About
-							</a>
-						</li>
-						<li>
-							<a href="#projects" className="nav__link">
-								Projects
-							</a>
-						</li>
-						<li>
-							<a href="#contact" className="nav__link">
-								Contact
-							</a>
-						</li>
-					</ul>
-				</section>
 				<Hamburger />
+				<ul className="nav__links">
+					<li>
+						<a href="#home" className="nav__link">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="#about" className="nav__link">
+							About
+						</a>
+					</li>
+					<li>
+						<a href="#projects" className="nav__link">
+							Projects
+						</a>
+					</li>
+					<li>
+						<a href="#contact" className="nav__link">
+							Contact
+						</a>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	);
